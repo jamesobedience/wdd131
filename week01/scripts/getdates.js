db2409a8146bcfbc28b633e91b256f2fcc58cff3
jamesobedience <ojames1@byupathway.edu>
2025-09-11 07:54:21 -0700

    001

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
